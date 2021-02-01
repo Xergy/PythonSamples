3e42c306167de9adf6751861863f57d4d1258cc9
Raffe Felts <Xergy@users.noreply.github.com>
2021-01-31 19:48:06 -0500

    Fun with Lists

--- FunWithLists.py
+++ FunWithLists.py
@@ -5,13 +5,20 @@ for i in range(0, 3):
     for ii in range(0, 3):
         for iii in range(0, 3):
             print(f'i = {i} ii = {ii} iii = {iii} ')
-            if (i == 1):
+            if (ii + iii == 3):
                 Result = "Yes!"
                 print (f'Yes!')
                 MyResultsList =  [i,ii,ii,Result]                
                 WhiteList.append(MyResultsList)
 
 print(WhiteList)
+
+# Get x's
+for item in WhiteList:
+    print(f'x = {item[0]} y = {item[1]} z = {item[2]} Result = {item[3]} ')
+
+    
+
 
 
         
